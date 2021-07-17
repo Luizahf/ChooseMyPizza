d507d746002663ff6ac849935d905b4ea8a655ee
Luíza Freire <luiza.freire@localiza.com>
2021-07-17 17:06:45 -0300

    Escolha de sabor Coroutine

--- app/src/main/java/com/example/choosemypizza/MainActivity.kt
+++ app/src/main/java/com/example/choosemypizza/MainActivity.kt
@@ -2,23 +2,51 @@ package com.example.choosemypizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.supportActionBar?.hide()
 
         setLayoutInicial()
+        val btnEscolher = findViewById<Button>(R.id.escolher)
+        btnEscolher.setOnClickListener(this)
+
+        MainViewModel.saborEscolhido.observe(this, { sabor ->
+            if (sabor != null) {
+                // TODO ir para tela final
+            }
+        })
     }
 
     private fun setLayoutInicial() {
         val tituloPagina = findViewById<TextView>(R.id.titulo)
-        tituloPagina.text = "Choose my Pizza!!"
+        tituloPagina.text = getString(R.string.titulo_escolha)
 
         val imgPizza = findViewById<ImageView>(R.id.pizza)
         imgPizza.setImageResource(R.drawable.escolher_pizza)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.escolher -> {
+                exibirLoad()
+                GlobalScope.launch {
+                    MainViewModel.escolherSabor()
+                }
+            }
+        }
+    }
+
+    private fun exibirLoad() {
+        val imgPizza = findViewById<ImageView>(R.id.pizza)
+        imgPizza.animate().rotation(1085F).start()
     }
 }
--- app/src/main/java/com/example/choosemypizza/SaboresRepositorio.kt
+++ app/src/main/java/com/example/choosemypizza/SaboresRepositorio.kt
@@ -1,4 +1,18 @@
 package com.example.choosemypizza
 
-class SaboresRepositorio {
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+
+
+object SaboresRepositorio {
+    suspend fun escolherSabor() : Int {
+        val quatro_queijos = R.drawable.quatro_queijos
+        val calabresa = R.drawable.calabresa
+        val aModa = R.drawable.a_moda
+        val portuguesa = R.drawable.portuguesa
+
+        val sabores = mutableListOf(quatro_queijos, calabresa, aModa, portuguesa)
+        val task = GlobalScope.async { sabores.random() }
+        return task.await()
+    }
 }
