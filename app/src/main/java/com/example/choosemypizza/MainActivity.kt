fce403fae0ca90fd05fd29359f0ddd32337a1743
Luíza Freire <luiza.freire@localiza.com>
2021-07-17 15:53:22 -0300

    Display image view

--- app/src/main/java/com/example/choosemypizza/MainActivity.kt
+++ app/src/main/java/com/example/choosemypizza/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.choosemypizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
+
+        setLayoutInicial()
+    }
+
+    private fun setLayoutInicial() {
+        val tituloPagina = findViewById<TextView>(R.id.titulo)
+        tituloPagina.text = "Choose my Pizza!!"
+
+        val imgPizza = findViewById<ImageView>(R.id.pizza)
+        imgPizza.setImageResource(R.drawable.escolher_pizza)
     }
 }
