dfdec3c4f5e7c055b885e56dda10dbb3952964bb
Luíza Freire <luiza.freire@localiza.com>
2021-07-17 17:34:05 -0300

    Imports

--- app/src/main/java/com/example/choosemypizza/MainActivity.kt
+++ app/src/main/java/com/example/choosemypizza/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.choosemypizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
